fc3992ee3c3ccfc58e4e461d61055ace684aec70
salunkevv <86159929+salunkevv@users.noreply.github.com>
2023-11-18 10:17:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+
+	let count=1;
+	for(let i=0;i<str.length-1;i++)
+		{
+			if(str.charAt(i)== str.charAt(i+1))
+			{
+				count++;
+			}
+			else
+			{
+				count--;
+			}
+			if(count==0)
+			{
+				return str.charAt(i);
+			}
+		}
+	return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
